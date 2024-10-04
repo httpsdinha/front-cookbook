4b4d17ab567a6290a9ba84db36df88475efb8c0d
Amanda Gois <amandabalcacar@gmail.com>
2024-10-04 16:52:46 -0400

    style: page home ok

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,7 +8,7 @@
   </div>
   <div class="right-container">
     <router-link to="/initial">
-      <button class="next">
+      <button class="next" @click="goToPage('/initial')">
        Avançar
       </button>
     </router-link>
@@ -18,7 +18,12 @@
   
 <script>
 export default {
-  name: "HomePage"
+  name: "TelaInicial",
+  methods: {
+    goToPage(route) {
+      this.$router.push(route);
+    },
+  },
 };
 </script>
 
@@ -36,6 +41,8 @@ body{
 .main-container{
   display: flex;
   height: 100vh;
+  overflow: hidden;
+  
 }
 
 /* Lado Esquerdo */
@@ -79,11 +86,15 @@ body{
 
 /* Lado Direito */
 .right-container{
-  width: 50%; 
+  background-image: url('../assets/wallpaper.jpg');
+  background-size: cover;
+  background-position: center;
+  width: 50%;
   display: flex;
   flex-direction: column;
   align-items: center; 
   justify-content: center; 
+  
 }
 
 .next{
@@ -94,8 +105,20 @@ body{
   box-shadow: -6px 8px 23.399999618530273px 3px rgba(0, 0, 0, 0.25); 
   border-radius: 5px; 
   border: 1px rgba(0, 0, 0, 0.26) solid;
-  margin-right: 10px;
-  
+  color: #D9D9D9;
+  font-size: 40px;
+  font-family: Imprima;
+  font-weight: 400;
+  word-wrap: break-word;
+  padding: 10px 20px;
+  cursor: pointer;
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+}
+
+.next:hover{
+  background: #A12A09;
 }
 
 </style>
